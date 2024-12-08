a3d7864170f24ce923ec1bd2a26b0cc281721fba
famisics <dev@uiro.dev>
2024-12-08 20:52:54 +0900

    test ci

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Flask! 0.0"
+    return "FLASH!!! from self-hosted"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
