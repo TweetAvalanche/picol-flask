be84b6048115f2b8f0e8a4ee16b23b2fb182c3c2
famisics <dev@uiro.dev>
2024-12-11 21:51:18 +0900

    imageをcharaに変更

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ def create_app():
 
     # ブループリントの登録
     from .root import root_bp
-    from .image import image_bp
+    from .chara import chara_bp
     app.register_blueprint(root_bp, url_prefix="/")
-    app.register_blueprint(image_bp, url_prefix="/image")
+    app.register_blueprint(chara_bp, url_prefix="/chara")
 
     return app
