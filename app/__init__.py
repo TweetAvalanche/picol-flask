c594c38c593c93afcff043c285a399b521f310ab
wisteriahuman <wisteria@ohyeah428.>
2024-12-12 17:17:12 +0900

    fix image関連のものを削除しました

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,9 @@ def create_app():
 
     # ブループリントの登録
     from .root import root_bp
-    from .image import image_bp
     from .user import user_bp
     from .chara import chara_bp
     app.register_blueprint(root_bp, url_prefix="/")
-    app.register_blueprint(image_bp, url_prefix="/image")
     app.register_blueprint(user_bp, url_prefix="/user")
     app.register_blueprint(chara_bp, url_prefix="/chara")
 
