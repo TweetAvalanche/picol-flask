b5ef70912b5f36f12aad6db3550c24af0c8b3f04
famisics <dev@uiro.dev>
2024-12-13 12:07:37 +0900

    add user_character

---
+++ app/user_character.py
@@ -0,0 +1,12 @@
+# ユーザーに紐づいたキャラクターを扱う
+
+def add_character(character_param, character_name):
+    return 0
+
+def get_character(cid):
+    return 0
+
+def update_character(cid, character_name):
+    return 0
+
+# デフォルトキャラ
