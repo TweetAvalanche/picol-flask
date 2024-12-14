155f7185abb742924285597326b8868dc73da4d7
famisics <dev@uiro.dev>
2024-12-15 00:40:04 +0900

    fix 循環インポートエラー

--- app/functions/character.py
+++ app/functions/character.py
@@ -1,8 +1,69 @@
 from mysql.connector import Error
 from .character_defs import generate_character
-from .user import get_user_function
 from ..mysql import get_db_connection
 import base64
+
+def get_user_function(uid):
+
+    # 値なしエラー
+    if not uid:
+        error_response = {"error": "Missing uid", "status": 400}
+        print(error_response)
+        return error_response
+    
+    # 型検証
+    if not isinstance(uid, int):
+        error_response = {"error": "uid must be an integer", "status": 400}
+        print(error_response)
+        return error_response
+
+    # データベースへの接続
+    conn = get_db_connection()
+    if isinstance(conn, tuple):
+        return conn  # エラーメッセージを返す
+
+    try:
+        cursor = conn.cursor(dictionary=True)
+        # パラメータをタプルとして渡すことで、SQLインジェクションを防ぐ
+        cursor.execute("SELECT * FROM users WHERE uid = %s", (uid,))
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if user:
+            cid = user['default_cid']
+            if cid == 0:
+                response = {
+                    "uid": uid,
+                    "user_message": user['message'],
+                    "cid": 0,
+                    "character_name": "",
+                    "character_param": "",
+                    "character_aura_image": "",
+                    "status": 200
+                }
+                print(response)
+                return response
+            else:
+                character = get_character_function(cid)
+                response = {
+                    "uid": uid,
+                    "user_message": user['message'],
+                    "cid": cid,
+                    "character_name": character['character_name'],
+                    "character_param": character['character_param'],
+                    "character_aura_image": character['character_aura_image'],
+                    "status": 200
+                }
+                print(response)
+                return response
+        else:
+            error_response = {"error": "user not found", "status": 404}
+            print(error_response)
+            return error_response
+    except Error as err:
+        error_response = {"error": str(err), "status": 500}
+        print(error_response)
+        return error_response
 
 # !キャラクターの追加
 def add_character_function(file, uid):
